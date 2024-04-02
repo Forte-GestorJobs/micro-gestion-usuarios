1d9c95e1a8e4795c4a07bf77b49984f2f6222f99
Mohamed11302 <mohamed.essalhi@alu.uclm.es>
2024-04-02 12:36:04 +0200

    docs: initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # micro-gestion-usuarios
+
+Microservicio para la gestión de los usuarios.
+Consta de una API con Spring Boot y una Base de Datos con Postgres
---
+++ micro-gestion-usuarios/src/main/java/nttdata/messalhi/forte/App.java
@@ -0,0 +1,13 @@
+package nttdata.messalhi.forte;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
