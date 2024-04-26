5a5e03867d168e3ef3dbb37ceb0caad01e696e26
Mohamed11302 <mohamed.essalhi@alu.uclm.es>
2024-04-26 09:21:32 +0200

    feat: Implementación endPoint para listar los usuarios

--- micro-gestion-usuarios/src/main/java/nttdata/messalhi/forte/http/UserDTORaceController.java
+++ micro-gestion-usuarios/src/main/java/nttdata/messalhi/forte/http/UserDTORaceController.java
@@ -2,6 +2,8 @@ package nttdata.messalhi.forte.http;
 import nttdata.messalhi.forte.auxi.UserDTO;
 import nttdata.messalhi.forte.services.UserDTORaceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,29 +14,38 @@ public class UserDTORaceController {
     @Autowired
     private UserDTORaceService userDTORaceService;
     @PostMapping("/User")
-    public ResponseEntity<String> addTaskSchedule(@RequestBody UserDTO userDTO) {
+    public ResponseEntity<String> addUser(@RequestBody UserDTO userDTO) {
         return this.userDTORaceService.addUser(userDTO);
     }
 
     @GetMapping("/User/{id}")
-    public ResponseEntity<String> getTaskSchedule(@PathVariable String id) {
+    public ResponseEntity<String> getUser(@PathVariable String id) {
         return this.userDTORaceService.getUser(id);
     }
 
+    @GetMapping("/User/list")
+    public ResponseEntity<String> listTaskSchedule(@RequestParam(defaultValue = "0") int page,
+                                                   @RequestParam(defaultValue = "10") int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return this.userDTORaceService.listUsers(pageable);
+    }
+
     @DeleteMapping("/User/{id}")
-    public ResponseEntity<String> deleteTaskSchedule(@PathVariable String id) {
+    public ResponseEntity<String> deleteUser(@PathVariable String id) {
         return this.userDTORaceService.deleteUser(id);
     }
 
     @PutMapping("/User")
-    public ResponseEntity<String> updateTaskSchedule(@RequestBody UserDTO userDTO){
+    public ResponseEntity<String> updateUser(@RequestBody UserDTO userDTO){
         return this.userDTORaceService.updateUser(userDTO);
     }
 
     @PostMapping("/User/checkUser")
-    public ResponseEntity<String> checkUserCredential(@RequestBody UserDTO user) {
+    public ResponseEntity<String> checkUser(@RequestBody UserDTO user) {
         return this.userDTORaceService.checkUser(user);
     }
+
+
 
 
 }
